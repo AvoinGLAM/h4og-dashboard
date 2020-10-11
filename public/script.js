260375867b5234c36e2c8434644c2774d290347a
Miksu <46541386+ahnl@users.noreply.github.com>
2020-10-11 15:01:44 +0300

    window.baseurl

--- public/script.js
+++ public/script.js
@@ -49,7 +49,7 @@ function openModal(e) {
         animateCSS('#modalCover', 'fadeIn');
         modalCover.style.display = 'block';
     
-        fetch(baseurl + '/modal?type=' + dataset.type + '&id=' + dataset.index)
+        fetch(window.baseurl + '/modal?type=' + dataset.type + '&id=' + dataset.index)
             .then(response => response.text())
             .then(html => {
                 modalOuter.innerHTML = html;
